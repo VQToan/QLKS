8406b09511c400be8bf9967530cc90c6607fe5ba
vqtoan1807 <vqtoan1807@gmail.com>
2020-12-18 20:37:52 +0700

    update 2.0
    remove login

--- src/GUI/App.java
+++ src/GUI/App.java
@@ -1,14 +1,20 @@
 package GUI;
 
+import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.Toolkit;
 
 public class App {
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					loginFrame frameLogin = new loginFrame();
-					frameLogin.setVisible(true);	   
+//					loginFrame frameLogin = new loginFrame();
+//					frameLogin.setVisible(true);
+					mainFrame frame= new mainFrame();
+					Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+					frame.setSize(screenSize.width, screenSize.height);
+					frame.setVisible(true);
 					}
 				 catch (Exception e) {
 					e.printStackTrace();
